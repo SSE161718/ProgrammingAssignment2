ba97fa8437e0a3380f725cc7f39967a507c20fac
SSE161718 <81488551+SSE161718@users.noreply.github.com>
2021-05-21 16:19:53 -0600

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  y <- x
+  m <- FALSE
+  getm <- function() m
+  setmatrix <- function(z){
+    y <<- z
+    m <<- TRUE
+  }
+  getmatrix <- function(){
+    y
+  }
+  list(getm = getm,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
+  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getm()
+  if(m == TRUE){
+    return(x$getmatrix())
+  }
+  t <- x$getmatrix()
+  tt <- solve(t);
+  x$setmatrix(tt)
+  tt
 }
